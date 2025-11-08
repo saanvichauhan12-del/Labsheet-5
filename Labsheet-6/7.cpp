a2d2f8260334db59a38dd48e07639e6c6cc22600
saanvichauhan12-del <saanvichauhan12@gmail.com>
2025-11-08 19:12:02 +0530

    Add matrix input and output functionality

---
+++ Labsheet-6/7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter number of rows: ";
+    cin >> r;
+    cout << "Enter number of columns: ";
+    cin >> c;
+
+    int arr[20][20];
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Matrix is:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
