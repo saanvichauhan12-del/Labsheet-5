7fb42b7ce68477bbc0b6619de0a5e08cdc2ee87f
saanvichauhan12-del <saanvichauhan12@gmail.com>
2025-11-10 18:56:22 +0530

    Create 9.cpp

---
+++ Labsheet-6/Labsheet-7/Labsheet-8/9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    swapNumbers(x, y);
+
+    cout << "After swapping: " << x << " " << y;
+    return 0;
+}
