28603a5add330cf63a6e59220cb5d6b79397507a
saanvichauhan12-del <saanvichauhan12@gmail.com>
2025-11-10 18:58:26 +0530

    Create 13.cpp

---
+++ Labsheet-6/Labsheet-7/Labsheet-8/13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+};
+
+class Student : public Person {
+public:
+    float marks;
+
+    void input() {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter age: ";
+        cin >> age;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void display() {
+        cout << name << " " << age << " " << marks;
+    }
+};
+
+int main() {
+    Student s;
+    s.input();
+    s.display();
+    return 0;
+}
